9196080f356a782489591aa7c2ae3b92f186e598
Juan Pablo Zamora Veraza <jupazave@gmail.com>
2018-01-24 22:52:47 -0600

    Added Adapter

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# keystone-fs-image-storage-adapter
-Keystone Storage Adapter to help you to manage an image document with Jimp tool
+# Jimp based image storage adapter for KeystoneJS
+
+Keystone Storage Adapter to help you to manage an image document with [Jimp](https://github.com/oliver-moran/jimp) tool
+
+
+# S3-based 
+
+This adapter is designed to manipulate images after upload with Jimp tool in KeystoneJS using the storage API.
+
+Tested in Node.js 6.12.2
+
+## Usage
+
+Configure the storage adapter:
+
+```js
+
+var storage = new keystone.Storage({
+    adapter: require('keystone-fs-image-storage-adapter'),
+    imagefs: {
+        path: keystone.expandPath('./uploads'), // required; path where the files should be stored
+        publicPath: '/uploads', // path where files will be served,
+        manageImage: function(file, callback) {
+            // Here you can manipulate file
+            callback(null, file);
+        }
+    }
+});
+```
+
+Then use it as the storage provider for a File field:
+
+```js
+File.add({
+  name: { type: String },
+  file: { type: Types.File, storage: storage },
+});
+```
+
+
+### Options
+
+The adapter requires an additional `fs` field added to the storage options. it accepts the following values:
+
+- **path**: _(string; required)_ Path the files will be stored at on disk
+
+- **generateFilename**: _(function; default: random filename)_ Method to generate a filename for the uploaded file. Gets passed the `file` data, the attempt number and the callback to call with the filename.
+  - See [`keystone-storage-namefunctions`](http://npm.im/keystone-storage-namefunctions) for additional filename generators, including content hash filename and original filename. See its source for more information on how to write your own.
+
+- **manageImage**: _(function; default: copies same file)_ Method to manipulate your image file
+  - See [`Jimp`](https://github.com/oliver-moran/jimp) for additional  documentation.
+
+
+- **whenExists**: _(string; default: 'retry')_ Specifies what to do when the file exists already. Can be one of `'retry'`, `'error'` or `'overwrite'`.
+
+- **retryAttempts**: _(number; default: 3)_ If `whenExists` is set to `'retry'`, how many times keystone should try to generate a unique filename before returning an error
+
+- **publicPath**: _(string)_ Optional path the files will served from by the webserver
+
+
+### Schema
+
+The FS adapter supports all the default Keystone file schema fields. It also additionally supports and enables the `filename` path (required).
+
+```JS
+{
+    filename: String,
+}
+```
+
+
+# License
+
+Licensed under the standard MIT license. See [LICENSE](license).
---
+++ index.js
@@ -0,0 +1,167 @@
+var assign = require('object-assign');
+var fs = require('fs-extra');
+var path = require('path');
+var url = require('url');
+var sanitize = require('sanitize-filename');
+var Jimp = require('jimp');
+var nameFunctions = require('keystone-storage-namefunctions');
+var ensureCallback = require('keystone-storage-namefunctions/ensureCallback');
+var prototypeMethods = require('keystone-storage-namefunctions/prototypeMethods');
+
+var debug = require('debug')('coatilabs:storage:adapter:imagefs');
+
+
+var DEFAULT_OPTIONS = {
+    generateFilename: nameFunctions.randomFilename,
+    whenExists: 'retry',
+    retryAttempts: 3, // For whenExists: 'retry'.
+    manageImage: function(file, callback) {
+        callback(null, file);
+    }
+};
+
+function ensurePath (path) {
+    // Ensure that the specified path exists and is writable. This is quick and
+    // happens on server startup, so sync functions are ok.
+    try {
+        // accessSync throws if the item doesn't exist or we don't have
+        // permission to read + write it.
+        fs.accessSync(path, fs.R_OK | fs.W_OK);
+
+        if (!fs.statSync(path).isDirectory()) {
+            throw Error('Specified output path is not a directory');
+        }
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            // Recover by creating the directory.
+            fs.mkdirsSync(path);
+            debug('Storage output path \'' + path + '\' created');
+            return;
+        }
+        throw e;
+    }
+}
+
+function FSImageAdapter (options, schema) {
+    if (!schema.filename) throw Error('Cannot use FSImageAdapter without storing filename');
+
+    this.options = assign({}, DEFAULT_OPTIONS, options.imagefs);
+    debug('Initialising FSImageAdapter with options', this.options);
+
+    this.options.generateFilename = ensureCallback(this.options.generateFilename);
+    this.manageImage = ensureCallback(this.options.manageImage);
+    ensurePath(this.options.path);
+}
+
+FSImageAdapter.compatibilityLevel = 1;
+
+// All the extra schema fields supported by this adapter.
+FSImageAdapter.SCHEMA_TYPES = {
+    // This adapter stores its key in the name of a file on disk.
+    filename: String,
+};
+
+FSImageAdapter.SCHEMA_FIELD_DEFAULTS = {
+    filename: true,
+};
+
+FSImageAdapter.prototype.getFilename = prototypeMethods.getFilename;
+FSImageAdapter.prototype.retryFilename = prototypeMethods.retryFilename;
+
+/**
+    Gets the public path of a stored file by combining the publicPath option
+    with the filename in the field value
+*/
+FSImageAdapter.prototype.getFileURL = function (file) {
+    var publicPath = this.options.publicPath;
+    if (!publicPath) return null; // No URL.
+
+    return url.resolve(publicPath, file.filename);
+};
+
+/**
+    Private function for getting the on-disk filename
+*/
+FSImageAdapter.prototype.pathForFile = function (filename) {
+    return path.resolve(this.options.path, sanitize(filename));
+};
+
+/**
+    Uploads a file at the specified path and returns the value to be stored
+    in the field value. The file argument must be an object as per the [multer
+    file information spec](https://github.com/expressjs/multer#file-information)
+*/
+FSImageAdapter.prototype.uploadFile = function (file, callback) {
+    var self = this;
+    debug('Uploading file', file);
+    var options = self.options;
+    self.getFilename(file, function (err, filename) {
+        if (err) return callback(err);
+        filename = sanitize(filename);
+        debug('Uploading file with filename: %s', filename);
+        var uploadPath = path.resolve(options.path, filename);
+        var fsOptions = {};
+        fsOptions.clobber = options.whenExists === 'overwrite';
+
+        Jimp.read(file.path, function (err, workingfile) {
+            if (err) return debug(err) && callback(err);
+            self.manageImage(workingfile, function(err, filetosave) {
+                if (err) return debug(err) && callback(err);
+                filetosave.write(file.path, function(){
+                    fs.move(file.path, uploadPath, fsOptions, function (err) {
+                        // TODO: Chmod the file.
+                        var data = {
+                            filename: filename,
+                            size: file.size,
+                            mimetype: file.mimetype,
+                            path: options.path,
+                            originalname: file.originalname,
+                        };
+                        debug('Uploaded file, returning data', data);
+                        callback(null, data);
+                    });
+                });
+            });
+        });
+
+
+    
+    });
+};
+
+FSImageAdapter.prototype.removeFile = function (file, callback) {
+    debug('Removing file', file);
+    fs.unlink(this.pathForFile(file.filename), function (err) {
+        if (err && err.code === 'ENOENT') {
+            // The file doesn't exist.
+            console.warn('Attempted to remove a non-existant file');
+            return callback();
+        }
+
+        callback(err);
+    });
+};
+
+FSImageAdapter.prototype.fileExists = function (filename, callback) {
+    var path = this.pathForFile(filename);
+    debug('Checking for file at path %s', filename);
+    // Returns (err, bool) to the callback based on whether or not the file
+    // already exists. Used if whenExists: 'error' or 'retry' in the options
+    fs.stat(path, function (err, stats) {
+        if (err && err.code === 'ENOENT') {
+            // File does not exist
+            callback(null, false);
+        } else if (err) {
+            // Other error getting file info
+            callback(err);
+        } else if (stats.isFile()) {
+            // File does exist
+            callback(null, true);
+        } else {
+            // Object at path is not a file
+            callback(Error('Invalid save destination - dest is not a file'));
+        }
+    });
+};
+
+module.exports = FSImageAdapter;
